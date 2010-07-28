a11ae4f7302b8831ce3b8b30db6365367f18102e
Ivan A-R <ivan+work@alferov.name>
2010-07-28 16:05:20 +0400

    UI: demo application

---
+++ firmware/apps/test-ui/main.c
@@ -0,0 +1,40 @@
+#include <ch.h>
+#include <hal.h>
+
+/*
+ * Red LEDs blinker thread, times are in milliseconds.
+ */
+static WORKING_AREA(waThread1, 128);
+static msg_t Thread1(void *arg) {
+
+  (void)arg;
+  while (TRUE) {
+    palClearPad(IOPORT3, GPIOC_LED);
+    chThdSleepMilliseconds(500);
+    palSetPad(IOPORT3, GPIOC_LED);
+    chThdSleepMilliseconds(500);
+  }
+  return 0;
+}
+
+/*
+ * Entry point, note, the main() function is already a thread in the system
+ * on entry.
+ */
+int main(int argc, char **argv) {
+
+  (void)argc;
+  (void)argv;
+
+  sdStart(&SD1, NULL);
+
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  while (TRUE) {
+    chThdSleepMilliseconds(500);
+  }
+  return 0;
+}
